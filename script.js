4ab41ec9c2d8ed5a67e2c5c11b804c156988fdc5
Ryan1ucas6 <204574272+Ryan1ucas6@users.noreply.github.com>
2025-06-08 14:41:48 +0100

    Completed JS code for calculator logic
    added some basic css

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ function multiply(a, b) {
     return a * b;
 }
  function divide(a, b) {
+    if(b === 0) {
+        return "Error: Can't divide by zero!"
+    }
     return a / b;
  }
 
@@ -29,3 +32,77 @@ function multiply(a, b) {
  }
 
 
+ const display = document.querySelector(".display")
+ const digitButtons = document.querySelectorAll(".digit")
+ const operatorButtons = document.querySelectorAll(".operator")
+ const equalButton = document.querySelector(".equal")
+ const clearButton = document.querySelector(".clear")
+
+ let firstOperand = "";
+ let secondOperand = "";
+ let currentOperator = null;
+ let shouldResetDisplay = false;
+
+digitButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const buttonValue = button.textContent;
+
+        if (shouldResetDisplay) {
+            if (buttonValue === ".") {
+                display.textContent = "0.";
+                shouldResetDisplay = false;
+                return;
+            }
+            display.textContent = "";
+            shouldResetDisplay = false;
+        }
+
+        if (buttonValue === "." && display.textContent.includes(".")) return;
+
+        display.textContent += buttonValue;
+    });
+});
+
+
+
+
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        if(currentOperator !== null && !shouldResetDisplay) {
+            const secondOperand = parseFloat(display.textContent);
+            const result = operate(currentOperator, firstOperand, secondOperand);
+            display.textContent = result;
+            firstOperand = result;
+        } else {
+            firstOperand = parseFloat(display.textContent);
+        }
+        currentOperator = button.textContent;
+        shouldResetDisplay = true;
+    })
+})
+
+equalButton.addEventListener("click", () => {
+    if(currentOperator === null || shouldResetDisplay) return;
+    const secondOperand = parseFloat(display.textContent);
+    const result = operate(currentOperator, firstOperand, secondOperand);
+    
+    if(typeof result === "string") {
+        display.textContent = result;
+        firstOperand = "";
+        currentOperator = null;
+        shouldResetDisplay = true;
+    } else {
+        display.textContent = result;
+        firstOperand = result;
+        currentOperator = null
+        shouldResetDisplay = true;
+    }
+})
+
+clearButton.addEventListener("click", () => {
+    display.textContent = "";
+    firstOperand = "";
+    secondOperand = "";
+    currentOperator = null;
+    shouldResetDisplay = false;
+})
